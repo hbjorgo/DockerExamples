55c50e2b526b9c963c9a95b4daca7d5dd54e7201
Henning Hultman Bjørgo <henning.bjorgo@easee.no>
2022-02-18 13:51:23 +0100

    Add init script

---
+++ dockerinitbaseimages.ps1
@@ -0,0 +1,4 @@
+docker pull mcr.microsoft.com/dotnet/sdk:6.0
+docker pull mcr.microsoft.com/dotnet/runtime:6.0
+docker pull mcr.microsoft.com/dotnet/runtime:6.0-alpine
+docker pull mcr.microsoft.com/dotnet/runtime-deps:6.0-alpine
